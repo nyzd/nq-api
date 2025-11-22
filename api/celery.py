70afc13a4ac36c515781c0cdc2b0990fc0ff82af
nyzd <abedi.mahdi.dev@gmail.com>
2025-11-22 16:49:42 +0330

    tasks Bug fix

--- api/celery.py
+++ api/celery.py
@@ -1,24 +1,14 @@
 import os
 from celery import Celery
 from django.conf import settings
+from kombu import Queue
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
 app = Celery('nq-api')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks() 
-
-app.conf.task_queues = {
-    'forced_alignment': {
-        'exchange': 'forced_alignment',
-        'routing_key': 'forcedalignment.processing',
-    },
-    'forced_alignment_done': {
-        'exchange': 'forced_alignment_done',
-        'routing_key': 'forcedalignment_done.processing',
-    },
-}
+app.autodiscover_tasks()
 
 app.conf.task_routes = {
     'quran.tasks.forced_alignment': {
